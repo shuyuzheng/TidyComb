af6a8a23f90bc1a3c00f7661d154f92d823aed16
Zheng <shuzhe@ad.helsinki.fi>
2019-03-13 09:33:07 +0200

    rename R code files

--- R/fit_dose_response_model.R
+++ R/fit_dose_response_model.R
@@ -89,3 +89,26 @@ CalculateIC50 <- function(coef, type, max.conc){
   return (ic50)
 
 }
+
+PredictResponse <- function(df, dose) {
+  if (stats::var(df$response, na.rm = TRUE) == 0) {
+    pred <- df$response[1]
+  } else {
+    model <- FitDoseResponse(df)
+
+    if (model$call$fct[[1]][[3]] == "LL.4") {
+      pred <- stats::predict(model, data.frame(dose = dose))
+    } else {
+      pred <- stats::predict(model, data.frame(dose = log(dose)))# NB! use log
+    }
+
+    if (pred > 100) {
+      pred <- 100 + stats::runif(1, -0.01, 0)
+    }
+  }
+  return(pred)
+}
+
+FitWholeMat <- function(response.mat) {
+
+}
