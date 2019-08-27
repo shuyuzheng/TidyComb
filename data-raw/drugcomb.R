20e47f35f335a738a5639a19dc720ea5c7f7318f
Shuyu <shuyu.zheng@helsinki.fi>
2019-08-27 17:28:58 +0300

    update "drugcomb" list in sysdata.rda

--- data-raw/drugcomb.R
+++ data-raw/drugcomb.R
@@ -1,5 +1,6 @@
 cell_line <- read.csv("data-raw/cell_line.csv",
                       stringsAsFactors = FALSE)
+colnames(cell_line) <- c("cellosaurus_accession", "id")
 drug <- read.csv("data-raw/drug.csv",
                  stringsAsFactors = FALSE)
 disease <- read.csv("data-raw/disease.csv",
